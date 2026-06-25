976d028ebb49a1d41fd6e75e7d94e36c5dc09d59
Inoriant <424504326@qq.com>
2026-06-25 19:42:07 +0800

    Keep framework and plugins on master

--- Assets/Scripts/Battle/Runtime/Skill/Executor/Base/IBattleSkillNodeExecutor.cs
+++
@@ -1,8 +0,0 @@
-namespace Battle
-{
-    /// <summary>Executor 统一接口，由 <see cref="SkillController"/> 调度。</summary>
-    public interface IBattleSkillNodeExecutor
-    {
-        void Execute(in SkillExecutionContext context);
-    }
-}
